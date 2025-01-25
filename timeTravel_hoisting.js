5add7fd9f167f8bc4351d04c53696cc338c4bb94
dcpomichter <dcpomichter@gmail.com>
2025-01-25 14:52:07 -0500

    created timeTravel_hoisting.js

---
+++ timeTravel_hoisting.js
@@ -0,0 +1,11 @@
+let destination = "Ancient Egypt"
+console.log(destination)
+destination = "Medieval Europe"
+console.log(destination)
+const travelDate = "2024-03-15"
+//travelDate = "2025-01-20"
+// Because 'travelDate' is a Constant simple variable and so it cannot be changed or reassigned.
+console.log(timeMachineModel)
+var timeMachineModel = "T-800"
+console.log(timeMachineModel)
+//When calling the 'timeMachineModel' before it is declared the code accepts that it is a variable put loads it as Undefined since it has not been given a value yet in the code.
