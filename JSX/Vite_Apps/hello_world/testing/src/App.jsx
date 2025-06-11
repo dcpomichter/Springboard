3359061e263c1f1808772f9997acdb4185486991
dcpomichter <dcpomichter@gmail.com>
2025-06-11 16:32:16 -0400

    updated files again
    Testing how best to create files for projects

--- JSX/Vite_Apps/hello_world/testing/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import '/Users/kpape/OneDrive/Documents/GitHub/Springboard/JSX/Vite_Apps/hello_world/testing/src/App.css'
-
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  )
-}
-
-export default App
