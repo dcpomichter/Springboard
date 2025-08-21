19921ca0c41c852f08b3d954b31c923cfbc77f40
dcpomichter <dcpomichter@gmail.com>
2025-08-20 22:28:31 -0400

    Updated Routes
    also added minor css

--- JSX/Vite_Apps/Space Travel/src/layouts/RootLayout.jsx
+++ JSX/Vite_Apps/Space Travel/src/layouts/RootLayout.jsx
@@ -1,7 +1,21 @@
+import styles from "./RootLayout.module.css"
 import React from 'react'
+import { NavLink, Outlet } from "react-router-dom"
 
 export default function RootLayout() {
   return (
-    <div>RootLayout</div>
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Space Travel</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="spacecrafts">Spacecrafts</NavLink>
+          <NavLink to="planets">Planets</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   )
 }
